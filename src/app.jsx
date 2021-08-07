beff598a5a45a361b3f3e4bd1775bf7bc8651fdc
jay <rladudrnjs12@gmail.com>
2021-08-07 15:55:32 +0900

    call video list & components setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,29 @@
-import './App.css';
+import "./index.css";
+import React, { useEffect, useState } from "react";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>:)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    console.log("useeffect");
+
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos/?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyACtU4lPRrpyklXCYqllRyc8mGBo8K_ywo",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []); // 마운트될때마다 호출되지 않도록 수정
+  // }, [name, videos]);
+  // name, video가 없데이트 될 때마다 호출
+  return <VideoList videos={videos} />;
 }
 
 export default App;
