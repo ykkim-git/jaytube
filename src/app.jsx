bbe83935840249ef62ab646482ce68f80d26c7ce
jay <rladudrnjs12@gmail.com>
2021-08-11 22:13:00 +0900

    add readme

--- src/app.jsx
+++ src/app.jsx
@@ -20,7 +20,8 @@ function App() {
       .then((response) => response.json())
       .then((result) => setVideos(result.items))
       .catch((error) => console.log("error", error));
-  }, []); // 마운트될때마다 호출되지 않도록 수정
+  }, []);
+  // []: 마운트될때마다 호출되지 않도록 수정
   // }, [name, videos]);
   // name, video가 없데이트 될 때마다 호출
   return <VideoList videos={videos} />;
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const VideoItem = (props) => (
+
       <h1>{props.video.snippet.title}</h1>
   );
 
