0826c65b7b3bf147f2e80af59e6f9616d490a99f
jay <rladudrnjs12@gmail.com>
2021-08-21 22:25:07 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -3,39 +3,18 @@ import React, { useEffect, useState } from "react";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search/?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyACtU4lPRrpyklXCYqllRyc8mGBo8K_ywo`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos/?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyACtU4lPRrpyklXCYqllRyc8mGBo8K_ywo",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
     // []: 마운트될때마다 호출되지 않도록 수정
     // }, [name, videos]);
     // name, video가 없데이트 될 때마다 호출
